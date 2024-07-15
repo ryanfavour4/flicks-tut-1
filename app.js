f78fba8daf6c5320ce535ad5a0f8b09adf90a53c
hawau <hawaubaraje@gmail.com>
2024-07-15 21:44:59 +0100

    fetch data

--- app.js
+++ app.js
@@ -3,41 +3,54 @@ const data = [];
 
 
 function getKeyStrokes(e) {
- const inputBox = document.querySelector("#inputbox");
- values = inputBox.value;
+    const inputBox = document.querySelector("#inputbox");
+    values = inputBox.value;
 };
 
 
 function submitEvent() {
- console.log(values);
- // post to an api with axios
- //! axios.post : create a new data
- //! axios.put | axios.patch : update or edit data
- //! axios.delete : to delete a data
- //! axios.get : get or fetch data from the api
+    console.log(values);
+    // post to an api with axios
+    //! axios.post : create a new data
+    //! axios.put | axios.patch : update or edit data
+    //! axios.delete : to delete a data
+    //! axios.get : get or fetch data from the api
 
- const objectPayload = {
-  firstName: values,
-  lastName: values,
-  phoneNumber: "+23400000000",
-  email: `${values}@gmail.com`,
-  message: values
- }
+    const objectPayload = {
+        firstName: values,
+        lastName: values,
+        phoneNumber: "+23400000000",
+        email: `${values}@gmail.com`,
+        message: values
+    }
 
- axios.post("https://emis-server.onrender.com/contact-lists", objectPayload)
-  .then(res => {
-   console.log(res);
-  }).catch(err => {
-   console.log(err);
-  })
+    axios.post("https://emis-server.onrender.com/contact-lists", objectPayload)
+        .then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+        })
 
- // axios.get("https://emis-server.onrender.com/contact-lists")
- //  .then(res => {
- //   console.log(res);
- //   data.push(res.data);
- //  }).catch(err => {
- //   console.log(err);
- //  })
+
+
+    axios.get("https://emis-server.onrender.com/contact-lists")
+        .then(res => {
+            console.log(res);
+            data = res.data;
+            displayData();
+        }).catch(err => {
+            console.log(err);
+        })
+
+    function displayData() {
+        const dataList = document.getElementById("data-list");
+        dataList.innerHTML = dataList.textContent;
+        data.forEach((item) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `Name: ${item.firstName} ${item.lastName}, Phone: ${item.phoneNumber}, Email: ${item.email}, Message: ${item.message}`;
+            dataList.appendChild(listItem);
+        });
+    }
 }
 
 
